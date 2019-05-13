7c1714d058b238f34e3fd0109a0ea3357895881d
jgrisham4 <jgrisham4@gmail.com>
2019-05-12 19:55:35 -0700

    Updating environment.

--- Environment.hs
+++ Environment.hs
@@ -8,27 +8,27 @@ module Environment
 --, speedOfSound
 
 -- R_air = 287.0 J / (kg * K)
-gasConstantAir = 287 :: (RealFloat a => a)
+gasConstantAir = 287.0
 
 -- gamma = cp / cv
-specificHeatRatio = 1.4 :: (RealFloat a => a)
+specificHeatRatio = 1.4
 
 -- Returns temperature in Kelvin when provided with altitude in meters.
-temperature :: RealFloat a => a -> a
+temperature :: Floating a => a -> a
 temperature h
   | h <= 11000 = 288.15 - 0.0065 * h
   | otherwise  = 216.0
 
 -- Returns pressure in Pascals when provided with altitude in meters.
-pressure :: RealFloat a => a -> a
+pressure :: Floating a => a -> a
 pressure h
-  | h <= 11000 = 101325 * (temperature h / 288.15) ** (5.2559)
-  | otherwise  = 22630 * (exp (-0.00015769 * (h - 11000)))
+  | h <= 11000 = 101325 * (temperature h / 288.15) ** 5.2559
+  | otherwise  = 22630 * exp (-0.00015769 * (h - 11000))
 
 -- Returns density in kg/m^3 when provided with altitude in meters.
-density :: RealFloat a => a -> a
-density h = (pressure h) / (gasConstantAir * (temperature h))
+density :: Floating a => a -> a
+density h = pressure h / (gasConstantAir * temperature h)
 
 -- Returns speed of sound in m/s when provided with altitude in meters.
---speedOfSound :: RealFloat a => a -> a
+--speedOfSound :: Floating a => a -> a
 --speedOfSound h = sqrt (specificHeatRatio * gasConstantAir * (temperature h))
