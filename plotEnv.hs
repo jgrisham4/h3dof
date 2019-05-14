4b820807bb53ebf427d1133fa141541397b42bf3
jgrisham4 <jgrisham4@gmail.com>
2019-05-13 20:29:22 -0700

    Updating plotEnv.

--- plotEnv.hs
+++ plotEnv.hs
@@ -1,8 +1,9 @@
 import Environment
 import SimUtils
+import Graphics.Gnuplot.Simple
 
 main = do
   let alt = (linspace 0 100000 100) :: [Double]
   let t = map temperature alt
   let p = map pressure alt
-  print t
+  plotList [] (zip alt t)
