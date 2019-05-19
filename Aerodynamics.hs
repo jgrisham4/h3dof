018e44c83639cb1307a6e96483db1bcb54279080
jgrisham4 <jgrisham4@gmail.com>
2019-05-18 21:39:36 -0700

    Ready to add function to integrate in time.

--- Aerodynamics.hs
+++ Aerodynamics.hs
@@ -1,12 +1,12 @@
-module Aero
+module Aerodynamics
 ( aerodynamicForce
 ) where
 
 aerodynamicForce :: Floating a => Velocity a -> a -> a -> Force a
-aerodynamicForce (Velocity v_BA_U) rho sref = Force (V.map (*drag) direction)
+aerodynamicForce (Velocity v_BA_B) rho sref = Force (V.map (*drag) direction)
   where
-    vinf = magnitude v_BA_U
+    vinf = magnitude v_BA_B
     q = 0.5 * rho * vinf^2
     cd = 1.5
     drag = q * sref * cd
-    direction = V.map (*(-1)) v_BA_U
+    direction = V.map (*(-1)) v_BA_B
--- CannonSim.hs
+++ CannonSim.hs
@@ -1,9 +1,9 @@
-import qualified Data.Vector as V
-import qualified Data.Map as DM
-import Control.Applicative
-import SimUtils
-import FileUtils
-import Interp
+import           Control.Applicative
+import qualified Data.Map            as DM
+import qualified Data.Vector         as V
+import           FileUtils
+import           Interp
+import           SimUtils
 
 {-
 B -> Body
@@ -11,6 +11,8 @@ A -> Air
 E -> ECEF
 U -> UEN
 -}
+
+-- Now need to define a function which steps the solution forward in time.
 
 main = do
   atmosphereData <- readDataFile "standard_atmosphere.dat" :: IO (DM.Map String (V.Vector Double))
